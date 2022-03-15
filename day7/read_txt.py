e3046e28ab1de34d76797f8375d47ca920fbe124
Neetu <neetusuthar90@gmail.com>
2022-03-15 19:41:42 +0530

    Hello

--- day7/read_txt.py
+++ day7/read_txt.py
@@ -1,10 +1,18 @@
-with open('accounts.txt',mode = 'r') as accounts:
-    print(f'{"Account" :<10} {"Name":<10} {"Balance" :>10}') #f string 
-    for rec in accounts:
-        account, name, balance = rec.split()
-        print(f'{account:<10} {name:<10} {balance:>10}')
+# with open('accounts.txt',mode = 'r') as accounts:
+#     print(f'{"Account" :<10} {"Name":<10} {"Balance" :>10}') #f string 
+#     for rec in accounts:
+#         account, name, balance = rec.split()
+#         print(f'{account:<10}{name:<10}{balance:>10}')
 
-        
-
+accounts = open('accounts.txt',mode = 'r')
+temp_file = open('temp_file.txt',mode = 'w')
+with accounts, temp_file:
+    for record in accounts:
+        account,name,balance = record.split()
+        if int(account) != 300:
+            temp_file.write(record)
+        else:
+            new_record = ' '.join([account, 'William', balance])
+            temp_file.write(new_record + '\n')
 
 
